ff07bdcb6bc590da3b43ab62f9d883fc120af5ec
hoak <hoak@offpiste.io>
2023-06-14 20:07:33 +0100

    updating devnet program id

--- lip-client/src/lib.rs
+++ lip-client/src/lib.rs
@@ -7,4 +7,4 @@ anchor_gen::generate_cpi_interface!(idl_path = "idl.json",);
 #[cfg(feature = "mainnet-beta")]
 declare_id!("9ZoxkdieCwUWu57SyNkd4PR2fb7azpNiefeZjwDbzBQo");
 #[cfg(not(feature = "mainnet-beta"))]
-declare_id!("9ZoxkdieCwUWu57SyNkd4PR2fb7azpNiefeZjwDbzBQo");
+declare_id!("F1HVQ92YoF27Z652KBETWoyagY7Vej6F6mtvKDvYK3rX");

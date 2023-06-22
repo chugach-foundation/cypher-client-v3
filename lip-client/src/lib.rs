ee89d005518ddb49d8e7e6324b1a9745ffe0d35b
hoak <hoak@offpiste.io>
2023-06-22 23:04:40 +0100

    updating lip pid on mainnet

--- lip-client/src/lib.rs
+++ lip-client/src/lib.rs
@@ -5,6 +5,6 @@ pub mod utils;
 anchor_gen::generate_cpi_interface!(idl_path = "idl.json",);
 
 #[cfg(feature = "mainnet-beta")]
-declare_id!("9ZoxkdieCwUWu57SyNkd4PR2fb7azpNiefeZjwDbzBQo");
+declare_id!("cLip5AGrwoNJaYxdNicRg6uXMZbVCNGvYPC3rKuyASS");
 #[cfg(not(feature = "mainnet-beta"))]
 declare_id!("F1HVQ92YoF27Z652KBETWoyagY7Vej6F6mtvKDvYK3rX");
